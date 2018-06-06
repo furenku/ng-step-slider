b4a3ac878279e2f92d3614fb77841a98276864b0
furenku <furenku@gmail.com>
2018-06-05 19:46:56 -0500

    step slider working with resize

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { StepSliderComponent } from './step-slider/step-slider.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StepSliderComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/step-slider/step-slider.component.ts
@@ -0,0 +1,102 @@
+import { Component, AfterViewInit, ViewChild, ViewChildren, ElementRef, QueryList, HostListener } from '@angular/core';
+import { Subscription, timer } from "rxjs";
+import { takeWhile } from "rxjs/operators";
+
+@Component({
+  selector: 'step-slider',
+  templateUrl: './step-slider.component.html',
+  styleUrls: ['./step-slider.component.scss']
+})
+export class StepSliderComponent implements AfterViewInit {
+
+  currentStep: number = 0;
+  numbers: number[];
+
+
+  itemWidth: number = 0; 
+  componentWidth: number = 0;
+
+  @ViewChild('wrapper') wrapper: ElementRef;
+  @ViewChildren('item') steps: QueryList<ElementRef>;
+
+  ngAfterViewInit() {
+
+    this.calculateValues() 
+    
+  } 
+  
+  calculateValues() {
+    this.componentWidth = this.wrapper.nativeElement.offsetWidth;
+    this.itemWidth = this.steps.toArray()[0].nativeElement.offsetWidth;
+  }
+  
+  constructor() { }
+
+  ngOnInit() {
+    this.numbers = Array(15).fill('').map((x,i)=>i);
+    this.itemWidth = 0;
+    this.componentWidth = 0;
+  }
+
+
+  scrollAmount: number;
+
+
+  shouldAnimate : boolean = false;
+  scrollTimer : Subscription; 
+
+
+
+  centerStep( i: number ) {
+    
+
+    let target = this.steps.toArray()[i].nativeElement
+    let wrapper = this.wrapper.nativeElement;
+    
+    this.shouldAnimate = true;
+    
+    if( !! this.scrollTimer ) {
+      this.scrollTimer.unsubscribe()
+    }
+
+    this.scrollTimer = timer(0,30).pipe(
+
+        takeWhile(() => this.shouldAnimate )).subscribe((e) => {
+          
+          let targetLeft;
+
+          targetLeft = Math.max( target.offsetLeft - this.componentWidth / 2 + this.itemWidth / 2, 0 );
+          targetLeft = Math.min( targetLeft, wrapper.scrollWidth - wrapper.offsetWidth );
+
+          if (wrapper.scrollLeft >= targetLeft ) {
+            this.scrollAmount = wrapper.scrollLeft - 10;
+          } else if (wrapper.scrollLeft <= targetLeft) {
+            this.scrollAmount = wrapper.scrollLeft + 10;
+          }
+                  
+        if (
+          
+          
+          wrapper.scrollLeft + 10 > targetLeft && wrapper.scrollLeft - 10 < targetLeft
+       
+        ) {
+
+          this.shouldAnimate = false;
+
+          this.currentStep = i;
+
+        }
+
+      });
+
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    this.calculateValues()
+    this.centerStep( this.currentStep );
+  }
+
+
+  
+}
